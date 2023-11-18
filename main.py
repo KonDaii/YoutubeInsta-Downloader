d6304ca786b6368a45f3cd4d8c8fcfe7f0ca1c4e
KonDaii <dannylao1101@gmail.com>
2023-11-18 15:19:38 -0500

    youtubeModule fix
    Reconnected main and youtubeModule

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # Author: Danny Lao
 # Date: Nov 17, 2023
 # YouTubeInstagram Downloader uses the pytube library to install YouTube videos and audio for personal use.
-# It was made as a personal tool to avoid having to use webites for each individual site you want to download from.
+# It was made as a personal tool to avoid having to use websites for each individual site you want to download from.
 
-from pytube import YouTube
 import youtubeModule
+
 
 def main():
     link = input("Enter the YouTube video URL: ")
@@ -12,9 +12,9 @@ def main():
         print("Do you want Video or just Audio only? ")
         videoOrAudio = input().lower()  # Convert the input to lowercase for case-insensitive comparison
         if videoOrAudio == "video":
-            DownloadYoutubeVideo(link)
+            youtubeModule.DownloadYoutubeVideo(link)
         elif videoOrAudio == "audio":
-            DownloadYoutubeAudio(link)
+            youtubeModule.DownloadYoutubeAudio(link)
         else:
             print("Please select either video or audio.")
     else:
