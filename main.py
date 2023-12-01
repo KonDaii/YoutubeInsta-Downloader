f1a3ce0ca1f848aabfaab56f3fb7ba8380967bd4
KonDaii <dannylao1101@gmail.com>
2023-11-30 23:27:31 -0500

    Starting instagramModule
    non-functioning

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 # Date: Nov 17, 2023
 # YouTubeInstagram Downloader uses the pytube library to install YouTube videos and audio for personal use.
 # It was made as a personal tool to avoid having to use websites for each individual site you want to download from.
-
+import instagramModule
 import youtubeModule
 
 
 def main():
-    link = input("Enter the YouTube video URL: ")
+    link = input("Enter the Instagram/YouTube video URL: ")
     if "youtube.com" in link:
-        print("Do you want Video or just Audio only? ")
+        print("Do you want video or audio only? ")
         videoOrAudio = input().lower()  # Convert the input to lowercase for case-insensitive comparison
         if videoOrAudio == "video":
             youtubeModule.DownloadYoutubeVideo(link)
@@ -17,8 +17,10 @@ def main():
             youtubeModule.DownloadYoutubeAudio(link)
         else:
             print("Please select either video or audio.")
+    elif "instagram.com" in link:
+        instagramModule.downloadReel(link)
     else:
-        print("The link is not a YouTube link")
+        print("The link is not a YouTube/Instagram link")
+
 
 main()
-
