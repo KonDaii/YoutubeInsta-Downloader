14e42e4a0adc1c99a615cf698b423d765b8f660c
KonDaii <dannylao1101@gmail.com>
2023-12-27 16:50:19 -0500

    GUI
    Added basic user interface

--- instagramModule.py
+++ instagramModule.py
@@ -1,5 +1,5 @@
-# instagramModule.py
 import instaloader
+from tkinter import Toplevel, Label
 
 
 def downloadReel(instagram_url):
@@ -11,13 +11,20 @@ def downloadReel(instagram_url):
         post = instaloader.Post.from_shortcode(loader.context, instagram_url.split("/")[-2])
 
         # Download the reel
+<<<<<<< Updated upstream
         loader.download_post(post, target='reels')
 
         print("Reel downloaded successfully.")
+=======
+        loader.download_post(post, target='InstagramReels')
+        print("Instagram reel has downloaded successfully.")
+        show_download_complete_window("Instagram Reel Download Complete")
+>>>>>>> Stashed changes
     except instaloader.exceptions.InstaloaderException as e:
         print(f"Error: {e}")
 
 
+<<<<<<< Updated upstream
 if __name__ == "__main__":
     # You can test the module by running it as a script
     link = input("Enter the Instagram reel URL: ")
@@ -26,3 +33,10 @@ if __name__ == "__main__":
     else:
         print("The link is not an Instagram reel link.")
 
+=======
+def show_download_complete_window(message):
+    window = Toplevel()
+    window.title("Download Complete")
+    label = Label(window, text=message)
+    label.pack()
+>>>>>>> Stashed changes
--- youtubeModule.py
+++ youtubeModule.py
@@ -1,19 +1,61 @@
 from pytube import YouTube
+<<<<<<< Updated upstream
+=======
+import os
+from tkinter import Toplevel, Label
+
+
+>>>>>>> Stashed changes
 def DownloadYoutubeVideo(link):
     youtubeVideo = YouTube(link)
     video = youtubeVideo.streams.get_highest_resolution()
     try:
+<<<<<<< Updated upstream
         video.download()
     except:
         print("An error has occurred")
     print("Video download is completed successfully")
+=======
+        # Create a folder if it doesn't exist
+        folder_path = 'YoutubeVideo'
+        os.makedirs(folder_path, exist_ok=True)
+
+        # Download video to the folder
+        video.download(output_path=folder_path)
+    except Exception as e:
+        print(f"An error has occurred: {e}")
+    else:
+        print("Youtube video download has completed successfully")
+        show_download_complete_window("Youtube Video Download Complete")
+>>>>>>> Stashed changes
 
 
 def DownloadYoutubeAudio(link):
     youtubeAudio = YouTube(link)
     audio = youtubeAudio.streams.get_audio_only()
     try:
+<<<<<<< Updated upstream
         audio.download()
     except:
         print("An error has occurred")
     print("Audio download is completed successfully")
+=======
+        # Create a folder if it doesn't exist
+        folder_path = 'YoutubeAudio'
+        os.makedirs(folder_path, exist_ok=True)
+
+        # Download audio to the folder
+        audio.download(output_path=folder_path)
+    except Exception as e:
+        print(f"An error has occurred: {e}")
+    else:
+        print("Youtube audio download has completed successfully")
+        show_download_complete_window("Youtube Audio Download Complete")
+
+
+def show_download_complete_window(message):
+    window = Toplevel()
+    window.title("Download Complete")
+    label = Label(window, text=message)
+    label.pack()
+>>>>>>> Stashed changes
