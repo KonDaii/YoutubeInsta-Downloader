e5af94d1a54b3929c37520f49ab7128c0b486c83
KonDaii <dannylao1101@gmail.com>
2024-01-14 15:31:41 -0500

    Reworked GUI + Browse Directory function for Youtube
    Made GUI inside a function and added directory selection for downloaded youtube videos. Does not work for Instagram reels currently

--- youtubeModule.py
+++ youtubeModule.py
@@ -3,15 +3,16 @@ import os
 from tkinter import Toplevel, Label
 
 
-def downloadYoutubeVideo(link):
+def downloadYoutubeVideo(link,downloadFolder):
+    # Create a folder if user does not specify
+    if downloadFolder == "":
+        folder_path = 'YoutubeVideo'
+        os.makedirs(folder_path, exist_ok=True)
+    else:
+        folder_path = downloadFolder
     try:
         youtubeVideo = YouTube(link)
         video = youtubeVideo.streams.get_highest_resolution()
-
-        # Create a folder if it doesn't exist
-        folder_path = 'YoutubeVideo'
-        os.makedirs(folder_path, exist_ok=True)
-
         # Download video to the folder
         video.download(output_path=folder_path)
 
@@ -21,34 +22,34 @@ def downloadYoutubeVideo(link):
         show_download_complete_window("Youtube Video Download Complete")
 
 
-def downloadYoutubeAudio(link):
+def downloadYoutubeAudio(link,downloadFolder):
+    # Create a folder if user does not specify
+    if downloadFolder == "":
+        folder_path = 'YoutubeAudio'
+        os.makedirs(folder_path, exist_ok=True)
+    else:
+        folder_path = downloadFolder
     try:
         youtubeAudio = YouTube(link)
         audio = youtubeAudio.streams.get_audio_only()
-
-        # Create a folder if it doesn't exist
-        folder_path = 'YoutubeAudio'
-        os.makedirs(folder_path, exist_ok=True)
-
         # Download audio to the folder
         audio.download(output_path=folder_path)
 
     except Exception as e:
         show_error_window(f"Audio is unavailable. Double check your link and try again.")
     else:
-
         show_download_complete_window("Youtube Audio Download Complete")
 
 
 def show_download_complete_window(message):
     window = Toplevel()
     window.title("Download Complete")
-    label = Label(window, text=message)
+    label = Label(window, text=message, bg="forest green")
     label.pack()
 
 
 def show_error_window(error_message):
     window = Toplevel()
     window.title("Error")
-    label = Label(window, text=error_message)
+    label = Label(window, text=error_message, bg="red")
     label.pack()
